e1a3c543711f1cf5544f94ed8b15e57cc16f5871
Louisa Barrett <louisabarrett@gmail.com>
2014-01-17 11:40:45 -0700

    first test passing, second test failing

--- checkerboard.rb
+++ checkerboard.rb
@@ -4,7 +4,19 @@ class Checkerboard
   end
 
   def to_s
-    rows = [['B', 'W'], ['W', 'B']]
+    rows = []
+
+    2.times do |i|
+    row = []
+      2.times do |j|
+        if i.even? && j.even? || i.odd? && j.odd?
+          row << "B"
+        else
+          row << "W"
+        end
+      end
+    rows << row
+    end
 
     rows.map do |row|
       row.join(' ') + "\n"
--- checkerboard_test.rb
+++ checkerboard_test.rb
@@ -13,4 +13,13 @@ BOARD
     assert_equal expected, Checkerboard.new(2).to_s
   end
 
+  def test_a_larger_board
+    expected = <<-BOARD
+B W B
+W B W
+B W B
+BOARD
+    assert_equal expected, Checkerboard.new(3).to_s
+  end
+
 end
